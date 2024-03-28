cd165c9e4f6adb662bffb700e83d1699f3768643
박성재 <bobsot0225@gmail.com>
2024-03-28 20:56:07 +0900

    14,15,16강 - 강의자료

--- class14-16/js/detail.js
+++ class14-16/js/detail.js
@@ -1,16 +1,16 @@
-const logoutBtn = document.querySelector(".page__header__button")
-logoutBtn.addEventListener("click", () => {
-    location.href = "intro.html"
+const logoutBtn = document.querySelector('.page__header__button')
+logoutBtn.addEventListener('click', () => {
+    location.href = 'intro.html'
 })
 
-const photoDialog = document.querySelector(".page__dialog")
-
-const widgetPhoto = document.querySelector(".page__body__widget.photo")
-widgetPhoto.addEventListener("click", () => {
-    photoDialog.classList.add("active")
+const photoDialog = document.querySelector('.page__dialog')
+const widgetPhoto = document.querySelector('.page__body__widget.photo')
+widgetPhoto.addEventListener('click', () => {
+    photoDialog.classList.add('active')
 })
 
-const closeBtn = document.querySelector(".section-left__header__button.red")
-closeBtn.addEventListener("click", () => {
-    photoDialog.classList.remove("active")
+// 다이얼로그의 빨간 버튼을 클릭했을 때, 이벤트 동작
+const closeBtn = document.querySelector('.section-left__header__button.red')
+closeBtn.addEventListener('click', () => {
+    photoDialog.classList.remove('active')
 })
